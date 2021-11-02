6cbc04eecc8abdb0fd4311b9c52740f939150483
tahoorqadri <88522789+tahoorqadri@users.noreply.github.com>
2021-11-02 18:49:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-#test
+"""
+  **** PYRATES CODING CLUB PROJECT ******
+********* QR CODE SCANNER ULTIMATE **********
+  ************* TAHOOR BILAL *************
+"""
 
+import cv2
+from pyzbar.pyzbar import decode
+img = cv2.imread("uploadthephoto.png")
+code=decode(img)
+print(code)
 
-
-
-#testt
